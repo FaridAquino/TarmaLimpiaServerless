48dba4b14f5c6ccfb6f45c70e6df31a1c2fb255b
FaridAquino <farid.aquino@utec.edu.pe>
2025-12-22 17:58:35 -0500

    login y register de basurero

--- Basurero/handler.py
+++ Basurero/handler.py
@@ -1,13 +1,15 @@
 import json
+import hmac
 import os
 import boto3
 import uuid
+import hashlib
 from decimal import Decimal
 
 RUTAS_TABLE = os.environ.get('RUTA_TABLE')
 UBICACION_BASURERO_TABLE= os.environ.get('UBICACION_BASURERO_TABLE')
-CONNECTIONS_TABLE = os.environ.get('CONNECTIONS_TABLE')
-UBICACIONES_USUARIOS_TABLE = os.environ.get('UBICACIONES_USUARIOS_TABLE')
+BASURERO_TABLE=os.environ.get('BASURERO_TABLE')
+
 def registrarRuta(event, context):
     try:
         if 'body' not in event or event['body'] is None:
@@ -63,3 +65,110 @@ def registrarRuta(event, context):
             'body': json.dumps({'error': str(e)})
         }
 
+def registerBasurero(event, context):
+    try:
+        if 'body' not in event or event['body'] is None:
+            return {'statusCode': 400, 'body': json.dumps({'error': 'Falta el body'})}
+
+        body = json.loads(event['body'])
+
+        nombre = body['nombre']
+        correo = body['correo']
+        contraseña = body['contraseña']
+
+        contrasena=body["contrasena"]
+
+        # Hashing de la contraseña
+        salt = os.urandom(16)
+        hash_bytes = hashlib.pbkdf2_hmac(
+            'sha256', 
+            contrasena.encode('utf-8'), 
+            salt, 
+            600000
+        )
+
+        contrasena_hash = salt.hex() + ":" + hash_bytes.hex()
+
+        basureroTable = boto3.resource('dynamodb').Table(BASURERO_TABLE)
+        
+        basureroJson = {
+            'tenant_id': nombre,
+            'uuid': correo,
+            'contrasena_hash': contrasena_hash
+        }
+        
+        basureroTable.put_item(Item=basureroJson)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Basurero registrado exitosamente'})
+        }
+
+    except KeyError as e:
+        return {'statusCode': 400, 'body': json.dumps({'error': f'Falta el campo: {str(e)}'})}
+    except Exception as e:
+        print(e)
+        return {'statusCode': 500, 'body': json.dumps({'error': str(e)})}
+
+def loginBasurero(event, context):
+    try:
+        if 'body' not in event or event['body'] is None:
+            return {'statusCode': 400, 'body': json.dumps({'error': 'Falta el body'})}
+
+        body = json.loads(event['body'])
+
+        nombre = body['nombre']
+        correo = body['correo']
+        contrasena = body["contrasena"]
+
+
+        basureroTable = boto3.resource('dynamodb').Table(BASURERO_TABLE)
+        
+        response = basureroTable.get_item(Key={'tenant_id': nombre, 'uuid': correo})
+        
+        if 'Item' not in response:
+            return {
+                'statusCode': 404,
+                'body': json.dumps({'error': 'Usuario no encontrado'})
+            }
+
+        contrasena_guardada = response['Item']['contrasena_hash']
+        
+        try:
+            salt_hex, hash_real_hex = contrasena_guardada.split(":")
+        except ValueError:
+            return {'statusCode': 500, 'body': json.dumps({'error': 'Error en datos de usuario'})}
+
+        salt_bytes = bytes.fromhex(salt_hex)
+
+        hash_intento = hashlib.pbkdf2_hmac(
+            'sha256',
+            contrasena.encode('utf-8'),
+            salt_bytes,
+            600000
+        )
+
+        hash_real_bytes = bytes.fromhex(hash_real_hex)
+        
+        if hmac.compare_digest(hash_intento, hash_real_bytes):
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'message': 'Login exitoso'})
+            }
+        else:
+            return {
+                'statusCode': 401,
+                'body': json.dumps({'error': 'Contraseña incorrecta'})
+            }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
+
+    except KeyError as e:
+        return {'statusCode': 400, 'body': json.dumps({'error': f'Falta el campo: {str(e)}'})}
+    except Exception as e:
+        print(e)
+        return {'statusCode': 500, 'body': json.dumps({'error': str(e)})}
